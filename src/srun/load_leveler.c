29e135736f7e5e81831a0f9e9daad09332b2cba3
Morris Jette <jette@schedmd.com>
2012-07-23 17:31:52 -0700

    Update library that a job links to for MPI jobs with LoadLeveler
    Modify the library that an application links to for MPI type jobs (match
    both "libmpi.so" and "libmpich.so").

--- src/srun/load_leveler.c
+++ src/srun/load_leveler.c
@@ -837,7 +837,7 @@ static char *_get_cmd_protocol(char *cmd)
 			break;
 		}
 	}
-	if (strstr(buf, "libmpi.so"))
+	if (strstr(buf, "libmpi.so") || strstr(buf, "libmpich.so"))
 		protocol = "mpi";
 	else if (strstr(buf, "libshmem.so"))
 		protocol = "shmem";
